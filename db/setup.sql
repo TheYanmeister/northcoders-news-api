f46ab6a8948e6165ec0b259ce0abd09b05dbecac
TheYanmeister <yannickvdh1@gmail.com>
2019-06-03 11:15:33 +0100

    initial commit

---
+++ db/setup.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS northcoders_news_api;
+CREATE DATABASE northcoders_news_api;
+
+DROP DATABASE IF EXISTS northcoders_news_api_test;
+CREATE DATABASE northcoders_news_api_test;
